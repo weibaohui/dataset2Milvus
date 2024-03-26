087dbc20dd6456df1b9783ad9bc13573de579955
weibaohui <weibaohui@yeah.net>
2024-03-26 18:42:25 +0800

    done

--- data.py
+++ data.py
@@ -1,22 +1,25 @@
 import pandas as pd
 from datasets import load_dataset
-class Data:
-    def get_data_sets(self, name):
-        # https://huggingface.co/datasets/AIatMongoDB/embedded_movies
-        dataset = load_dataset("ComponentSoft/k8s-kubectl")
 
-        # Convert the dataset to a pandas dataframe
-        dataset_df = pd.DataFrame(dataset["train"])
 
-        dataset_df = dataset_df.dropna(subset=["command"])
-        print("\nNumber of missing values in each column after removal:")
-        print(dataset_df.isnull().sum())
+def get_data_sets(name):
+    # https://huggingface.co/datasets/AIatMongoDB/embedded_movies
+    dataset = load_dataset("ComponentSoft/k8s-kubectl")
 
-        dataset_df = dataset_df.drop(columns=["chain_of_thought"])
-        # print(dataset_df.head(5))
+    # Convert the dataset to a pandas dataframe
+    dataset_df = pd.DataFrame(dataset["train"])
 
-        # dataset_df = dataset_df.head(5)
-        dataset_df["strs"] = dataset_df["question"] + ";" + dataset_df["description"]
-        # dataset_df["vector"] = dataset_df["strs"].apply(self.get_embedding)
-        print(dataset_df.columns)
-        return dataset_df.to_dict("records")
+    dataset_df = dataset_df.dropna(subset=["command"])
+    print("\nNumber of missing values in each column after removal:")
+    print(dataset_df.isnull().sum())
+
+    dataset_df = dataset_df.drop(columns=["chain_of_thought"])
+    # print(dataset_df.head(5))
+
+    # dataset_df = dataset_df.head(5)
+    dataset_df["strs"] = dataset_df["question"] + ";" + dataset_df["description"]
+    # dataset_df["vector"] = dataset_df["strs"].apply(self.get_embedding)
+    print(dataset_df.columns)
+    return dataset_df.to_dict("records")
+
+
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from peewee import chunked
+import json
+
 from pymilvus import MilvusClient, DataType
 
 import SqliteDataBase
 import Transformer
-import data
 from milvus_helper import MilvusHelper
 
 
@@ -17,13 +17,13 @@ def create_collection(client: MilvusClient, collection_name: str):
     # 3.2. Add fields to schema
     schema.add_field(field_name="id", datatype=DataType.INT64, is_primary=True)
     schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=1024)
-    schema.add_field(field_name="objective", datatype=DataType.VARCHAR, max_length=1000, is_nullable=True)
-    schema.add_field(field_name="command_name", datatype=DataType.VARCHAR, max_length=1000, is_nullable=True)
-    schema.add_field(field_name="command", datatype=DataType.VARCHAR, max_length=1000, is_nullable=True)
-    schema.add_field(field_name="description", datatype=DataType.VARCHAR, max_length=1000, is_nullable=True)
-    schema.add_field(field_name="syntax", datatype=DataType.VARCHAR, max_length=1000, is_nullable=True)
-    schema.add_field(field_name="flags", datatype=DataType.VARCHAR, max_length=1000, is_nullable=True)
-    schema.add_field(field_name="question", datatype=DataType.VARCHAR, max_length=1000, is_nullable=True)
+    schema.add_field(field_name="objective", datatype=DataType.VARCHAR, max_length=65535, is_nullable=True)
+    schema.add_field(field_name="command_name", datatype=DataType.VARCHAR, max_length=65535, is_nullable=True)
+    schema.add_field(field_name="command", datatype=DataType.VARCHAR, max_length=65535, is_nullable=True)
+    schema.add_field(field_name="description", datatype=DataType.VARCHAR, max_length=65535, is_nullable=True)
+    schema.add_field(field_name="syntax", datatype=DataType.VARCHAR, max_length=65535, is_nullable=True)
+    schema.add_field(field_name="flags", datatype=DataType.VARCHAR, max_length=65535, is_nullable=True)
+    schema.add_field(field_name="question", datatype=DataType.VARCHAR, max_length=65535, is_nullable=True)
     # 3.3. Prepare index parameters
     index_params = client.prepare_index_params()
 
@@ -44,35 +44,81 @@ def create_collection(client: MilvusClient, collection_name: str):
     pass
 
 
-if __name__ == '__main__':
-    # 创建milvus数据库
-    # create_db('book')
-    # db_name = 'book'
-    # helper = MilvusHelper(host='127.0.0.1', port='19530', db_name=db_name)
-    # helper.drop_collection('book')
-    # create_collection(helper.client, 'book')
-    # helper.describe_collection('book')
-    # helper.load_collection('book')
-
-    # 获取数据集
+def import_dataset_to_milvus():
+    # 1.获取数据集
     # data = data.Data()
     # items = data.get_data_sets("book")
     # print(items[0])
-
-    # 将数据暂存到sqlite中，后面逐条计算向量，然后逐条更新，重启后可恢复进度
+    # 2.将数据暂存到sqlite中，后面逐条计算向量，然后逐条更新，重启后可恢复进度
     # SqliteDataBase.batch_save_items(items)
-
-    # 获取Vector 为null 的数据进行处理
-    items = SqliteDataBase.list_vector_null_items()
-    print(len(items))
-    transformer = Transformer.Transformer()
+    # 3.获取Vector 为null 的数据进行处理
+    # items = SqliteDataBase.list_vector_null_items()
+    # print(len(items))
+    # transformer = Transformer.Transformer()
+    # for item in items:
+    #     vector = transformer.get_embedding(item.strs)
+    #     item.vector = vector
+    #     (SqliteDataBase.Commands.update(
+    #         {
+    #             SqliteDataBase.Commands.vector: vector
+    #         }
+    #     )
+    #      .where(SqliteDataBase.Commands.id == item.id)
+    #      .execute())
+    # 4.将sqlite中的数据，导入到milvus 中
+    # 4.1创建milvus数据库
+    db_name = 'book'
+    helper = MilvusHelper(host='127.0.0.1', port='19530', db_name=db_name)
+    # helper.create_db('book')
+    helper.drop_collection('book')
+    create_collection(helper.client, 'book')
+    helper.describe_collection('book')
+    # 4.2 从sqlite中查询数据,插入milvus
+    items = SqliteDataBase.Commands.select().dicts()
+    items = list(items)
     for item in items:
-        vector = transformer.get_embedding(item.strs)
-        item.vector = vector
-        (SqliteDataBase.Commands.update(
-            {
-                SqliteDataBase.Commands.vector: vector
-            }
-        )
-         .where(SqliteDataBase.Commands.id == item.id)
-         .execute())
+        del item['id']
+        del item['strs']
+        item['vector'] = json.loads(item['vector'])
+        # None 类型转换为str
+        if item['question'] is None or len(item['question']) == 0:
+            item['question'] = ''
+        try:
+            helper.insert('book', item)
+        except Exception as e:
+            print(item)
+            print(e)
+    # 4.3 查看milvus中的数据
+    helper.describe_collection('book')
+    helper.load_collection('book')
+
+
+if __name__ == '__main__':
+    # import_dataset_to_milvus()
+
+    db_name = 'book'
+    helper = MilvusHelper(host='127.0.0.1', port='19530', db_name=db_name)
+    transformer = Transformer.Transformer()
+    search_text = 'Create a new namespace named kube-ingress'
+    print(f'Q:\t{search_text}')
+    search_vector = transformer.get_embedding(search_text)
+    res = helper.client.search(
+        collection_name='book',  # Replace with the actual name of your collection
+        # Replace with your query vector
+        data=[search_vector],
+        limit=1,  # Max. number of search results to return
+        search_params={"metric_type": "IP", "params": {}}  # Search parameters
+    )
+
+    # Convert the output to a formatted JSON string
+    result = json.dumps(res, indent=4)
+    # print(result)
+    ids_list = [item['id'] for item in res[0]]
+    # print(ids_list)
+
+    res = helper.client.get(
+        collection_name='book',
+        ids=ids_list
+    )
+    for item in res:
+        print(f'A:\t{item['command']} \t {item['flags']}')
